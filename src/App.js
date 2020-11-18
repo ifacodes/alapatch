f679c3324b94fad29ea27c8f74b2f129d0f77f17
[poww10s] <[theironminer@gmail.com]>
2020-11-18 03:05:37 -0500

    remove redux example counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import { Timbre } from './features/timbre/Timbre';
 import './App.css';
 
@@ -9,7 +8,6 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <Counter />
                 <Timbre />
             </header>
         </div>
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import parameterReducer from '../features/timbre/parameters/parameterSlice';
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         parameters: parameterReducer,
     },
 });
