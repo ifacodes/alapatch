da851966416090baed436e361eb52faf53ca2fd8
[poww10s] <[theironminer@gmail.com]>
2020-11-18 18:01:55 -0500

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Timbre } from './features/timbre/Timbre';
 import './App.css';
 
@@ -7,7 +6,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <Timbre />
             </header>
         </div>
