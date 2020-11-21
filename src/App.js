757ac1a52a054cce809223381d700a15d1fb306c
Aoife <aoifeebradley@gmail.com>
2020-11-21 05:05:53 -0500

    add basic menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import MainMenu from './features/menu/Menu';
 import TimbreSwitcher from './features/timbre/timbreSwitcher';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <TimbreSwitcher />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MainMenu />
+        <TimbreSwitcher />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/features/menu/Menu.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import logo from '../../1f3b9.svg';
+import './Menu.css';
+
+export default function MainMenu(props) {
+  const [open, setOpen] = useState(true);
+
+  const HandleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div>
+      <div className={`menu menu-${open}`}>
+        <img src={logo} className="menu-logo" alt="logo" />
+        <header className="menu-title">Alapatch</header>
+        <div className="menu-button-container">
+          <button className="menu-button" onClick={HandleClick}>
+            New Patch
+          </button>
+          <button className="menu-button">Load Patch</button>
+        </div>
+      </div>
+      {!open && (
+        <button className="menu-open-button" onClick={HandleClick}></button>
+      )}
+    </div>
+  );
+}
