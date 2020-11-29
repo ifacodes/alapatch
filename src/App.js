de4808f1a7fb1aac887164755b91aa347e181830
[poww10s] <[theironminer@gmail.com]>
2020-11-29 16:51:38 -0500

    formatting changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className='App'>
+            <header className='App-header'>
                 <MainMenu />
                 <TimbreSwitcher />
             </header>
